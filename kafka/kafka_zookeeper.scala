4e9cb6bf548689b0a9376b4c0732848d5984047a
Mahan Das <mahandas17@gmail.com>
2020-11-15 13:28:27 -0600

    zookeeper

---
+++ kafka/kafka_zookeeper.scala
@@ -0,0 +1,10 @@
+// Databricks notebook source
+// MAGIC %sh java -version
+// MAGIC ls -ltr ./
+// MAGIC wget https://archive.apache.org/dist/kafka/0.10.2.2/kafka_2.10-0.10.2.2.tgz
+// MAGIC tar -xzf kafka_2.10-0.10.2.2.tgz
+// MAGIC kafka_2.10-0.10.2.2/bin/zookeeper-server-start.sh kafka_2.10-0.10.2.2/config/zookeeper.properties
+
+// COMMAND ----------
+
+
